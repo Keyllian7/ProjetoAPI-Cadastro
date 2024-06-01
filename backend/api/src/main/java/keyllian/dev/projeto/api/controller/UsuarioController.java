7cffd6df144b4a06c31faf24f9bd8e2af3e01a14
Keyllian Azevedo <157046442+Keyllian7@users.noreply.github.com>
2024-06-01 15:33:34 -0300

    Criação dos Métodos, post, put e delete

--- backend/api/src/main/java/keyllian/dev/projeto/api/controller/UsuarioController.java
+++ backend/api/src/main/java/keyllian/dev/projeto/api/controller/UsuarioController.java
@@ -3,19 +3,39 @@ package keyllian.dev.projeto.api.controller;
 import keyllian.dev.projeto.api.dao.InterfaceUsuario;
 import keyllian.dev.projeto.api.model.Usuario;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
+@RequestMapping("/usuarios")
 public class UsuarioController {
 
     @Autowired
     private InterfaceUsuario dao;
 
-    @GetMapping("/usuarios")
+    @GetMapping
     public List<Usuario> listarUsuarios () {
         return (List<Usuario>) dao.findAll();
     }
+
+    @PostMapping
+    public Usuario adicionarUsuario (@RequestBody Usuario usuario) {
+        Usuario usuarioNovo = dao.save(usuario);
+        return usuarioNovo;
+    }
+
+    @PutMapping
+    public Usuario atualizarUsuario (@RequestBody Usuario usuario) {
+        return dao.save(usuario);
+    }
+
+    @DeleteMapping("/{id}")
+    public Optional<Usuario> deletarUsuario (@PathVariable Integer id) {
+        Optional<Usuario> usuario = dao.findById(id);
+        dao.deleteById(id);
+        return usuario;
+    }
+
 }
