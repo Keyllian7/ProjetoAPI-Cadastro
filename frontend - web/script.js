60a4f7b3925d6324c9ddf8cf2eca648af774b6c4
Keyllian Azevedo <157046442+Keyllian7@users.noreply.github.com>
2024-06-01 10:50:34 -0300

    Criação JavaScript

---
+++ frontend - web/script.js
@@ -0,0 +1,42 @@
+const formulario = document.querySelector("form");
+
+const Inputnome = document.querySelector(".nome");
+const Inputemail = document.querySelector(".email");
+const Inputsenha = document.querySelector(".senha");
+const Inputtelefone = document.querySelector(".telefone");
+
+function cadastrar() {
+
+    fetch("http://localhost:8080/cadastrar",
+    {
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        method: "POST",
+        body: JSON.stringify({
+            nome: Inputnome.value,
+            email: Inputemail.value,
+            senha: Inputsenha.value,
+            telefone: Inputtelefone.value
+        })
+    })
+    .then(function (res) { console.log(res) })
+    .catch(function (res) { console.log(res) })
+
+};
+
+function limpar () {
+    Inputnome.value = "";
+    Inputemail.value = "";
+    Inputsenha.value = "";
+    Inputtelefone.value = "";
+};
+
+formulario.addEventListener('submit', function (event) {
+
+    event.preventDefault();
+
+    cadastrar();
+    limpar();
+});
